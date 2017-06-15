83fcb95e390a860ba705d2051f3deeb102cce892
littleflute <13699175164@sina.cn>
2017-06-15 15:46:47 -0500

    Add files via upload

---
+++ Tango in the Night/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Big Love.mp3";		
+f[2]	= "02 Seven Wonders.mp3";		
+f[3]	= "03 Everywhere.mp3";		
+f[4]	= "04 Caroline.mp3";		
+f[5]	= "05 Tango in the Night.mp3";		
+f[6]	= "06 Mystfied.mp3";		
+f[7]	= "07 Little Lies.mp3";		
+f[8]	= "08 Family Man.mp3";		
+f[9]	= "09 Welcome to the Room...Sara.mp3";		
+f[10]	= "10 Isn't It Midnight.mp3";		
+f[11]	= "11 When I See You Again.mp3";		
+f[12]	= "12 You and I, Pt. 2.mp3";		
